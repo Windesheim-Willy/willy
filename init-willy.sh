1f68a81ae587e7669b06b64206f8532bcea7c54e
Dylan Reimerink <97.dylan@gmail.com>
2018-12-04 17:22:26 +0100

    Added init and run shell scripts

--- init-willy.sh
+++ init-willy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-./components/lidar/init.sh &&
+sudo chmod 777 /dev/willy_driver
+sudo chmod 777 /dev/input/js0
 
+CURDIR=$(pwd)
+cd components/lidar
+./init.sh
+cd "$CURDIR"
--- start-willy.sh
+++ start-willy.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-trap 'kill %1; kill %2; kill %3' SIGINT #Kill all processes if one recieves a kill signal
-roscore & # Start the ros master
-./components/navigation_stack/start-live-navstack.sh & # Start the nav stack
-./components/lidar/run.sh # Start the lidar capture
-# TODO start joystick
-# TODO start motor driver
+trap 'killall' INT
+
+killall() {
+	for pid in run/*.pid; do start-stop-daemon --stop --pidfile "$pid" --remove-pidfile ; done
+}
+
+start-stop-daemon --start --pidfile run/roscore.pid --make-pidfile --exec /opt/ros/kinetic/bin/roscore &
+sleep 2 && start-stop-daemon --start --pidfile run/nav_stack.pid --make-pidfile --exec /opt/ros/kinetic/bin/roslaunch -- $(pwd)/components/navigation_stack/navigation.launch path:=$(pwd)/components/navigation_stack &
+sleep 2 && start-stop-daemon --start --pidfile run/lidar.pid --make-pidfile --exec $(pwd)/components/lidar/run.sh &
+sleep 2 && start-stop-daemon --start --pidfile run/joystick.pid --make-pidfile --exec /opt/ros/kinetic/bin/roslaunch -- teleop_twist_joy teleop.launch &
+sleep 2 && start-stop-daemon --start --pidfile run/motor_driver.pid --make-pidfile --exec /opt/ros/kinetic/bin/rosrun -- rosserial_python serial_node.py _port:=/dev/willy_driver &
+
+cat
+

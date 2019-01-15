c9cc737eed7b77383b32d8953707cb6112219caf
willy <willy@windesheim.nl>
2019-01-15 16:29:48 +0100

    Update willy

--- init-willy.sh
+++ init-willy.sh
@@ -4,7 +4,7 @@ sudo chmod 777 /dev/willy_driver
 #sudo chmod 777 /dev/willy_openmv
 #sudo chmod 777 /dev/willy_sonar
 sudo chmod 777 /dev/input/js0
-sudo chmod 777 /dev/willy_camera
+sudo chmod 777 /dev/video0
 
 CURDIR=$(pwd)
 cd components/lidar
